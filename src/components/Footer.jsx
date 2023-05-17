801b22ce74e7bb30d68730ba5011a34ee11ab5d2
DariaLevandovschi <daria.levandovschi@iis.utm.md>
2023-05-17 14:04:38 +0300

    Routing and components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Layout } from "antd";
+const { Footer } = Layout;
+
+const FooterM = () => {
+  return (
+    <div>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Ant Design ©2023 Created by Ant UED
+      </Footer>
+    </div>
+  );
+};
+
+export default FooterM;
